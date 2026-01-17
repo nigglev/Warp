c570937a58b0e5dd515a82b22547168b91fb61d9
Andrey Ekimov <isuroku@gmail.com>
2026-01-17 19:41:35 +0100

    Moving of content layer

--- Source/Warp/CampaignMap/MapViewportWidget.cpp
+++ Source/Warp/CampaignMap/MapViewportWidget.cpp
@@ -3,6 +3,10 @@
 
 #include "MapViewportWidget.h"
 #include "MGLogs.h"
+#include "Blueprint/WidgetBlueprintLibrary.h"
+#include "Components/Border.h"
+#include "Components/CanvasPanel.h"
+
 
 DEFINE_LOG_CATEGORY_STATIC(AMapViewportWidgetLog, Log, All);
 
@@ -10,7 +14,88 @@ void UMapViewportWidget::NativeConstruct()
 {
 	Super::NativeConstruct();
 	
-	MG_COND_ERROR_SHORT(AMapViewportWidgetLog, !InputCatcher);
-	MG_COND_ERROR_SHORT(AMapViewportWidgetLog, !MapBorder);
-	MG_COND_ERROR_SHORT(AMapViewportWidgetLog, !MapContentRoot);
+	RETURN_ON_FAIL(AMapViewportWidgetLog, InputCatcher);
+	RETURN_ON_FAIL(AMapViewportWidgetLog, MapBorder);
+	RETURN_ON_FAIL(AMapViewportWidgetLog, MapContentRoot);
+	
+	InputCatcher->OnMouseButtonDownEvent.BindUFunction(this,
+		GET_FUNCTION_NAME_CHECKED(UMapViewportWidget, OnCatcherMouseDown));
+
+	InputCatcher->OnMouseMoveEvent.BindUFunction(this,
+		GET_FUNCTION_NAME_CHECKED(UMapViewportWidget, OnCatcherMouseMove));
+
+	InputCatcher->OnMouseButtonUpEvent.BindUFunction(this,
+		GET_FUNCTION_NAME_CHECKED(UMapViewportWidget, OnCatcherMouseUp));
+}
+
+void UMapViewportWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	Super::NativeTick(MyGeometry, InDeltaTime);
+	
+	//const FGeometry& BorderGeo = MapBorder->GetCachedGeometry();
+	//FVector2f ViewSize = BorderGeo.GetLocalSize();
+	
+	CurrentOffset_ = FMath::Vector2DInterpTo(CurrentOffset_, TargetOffset_, InDeltaTime, InterpSpeed_);
+	MapContentRoot->SetRenderTranslation(CurrentOffset_);
+}
+
+FEventReply UMapViewportWidget::OnCatcherMouseDown(FGeometry Geo, const FPointerEvent& E)
+{
+	if (E.GetEffectingButton() != EKeys::LeftMouseButton)
+	{
+		return UWidgetBlueprintLibrary::Unhandled();
+	}
+
+	
+	TSharedPtr<SWidget> pInputCatcherWidget = InputCatcher->GetCachedWidget();
+	if (!pInputCatcherWidget.IsValid())
+		return UWidgetBlueprintLibrary::Unhandled();
+	
+	bMouseDown_=true; 
+	PressPos_ = Geo.AbsoluteToLocal(E.GetScreenSpacePosition());
+	LastPos_ = PressPos_;
+	TargetOffset_ = CurrentOffset_;
+	
+	MG_LOG(AMapViewportWidgetLog, TEXT("PressPos: %s"), *PressPos_.ToString());
+	
+	FEventReply Reply(true);
+	Reply.NativeReply = FReply::Handled().CaptureMouse(InputCatcher->TakeWidget());
+	return Reply;	
+}
+
+FEventReply UMapViewportWidget::OnCatcherMouseMove(FGeometry Geo, const FPointerEvent& E)
+{
+	if (!bMouseDown_)
+	{
+		return UWidgetBlueprintLibrary::Unhandled();
+	}
+	
+	FVector2f LocalPos = Geo.AbsoluteToLocal(E.GetScreenSpacePosition());
+	FVector2f DraggingDelta = LocalPos - PressPos_;
+	
+	if (!bDragging_ && DraggingDelta.SquaredLength() > FMath::Square(DragThreshold_))
+	{
+		bDragging_ = true;
+	}
+	
+	if (bDragging_)
+	{
+		FVector2f Delta = LocalPos - LastPos_;
+		TargetOffset_ += FVector2D(Delta);
+		//TargetOffset_ = ClampOffset(TargetOffset_);
+	}
+	
+	LastPos_ = LocalPos;
+	
+	return UWidgetBlueprintLibrary::Handled();
+}
+
+FEventReply UMapViewportWidget::OnCatcherMouseUp(FGeometry Geo, const FPointerEvent& E)
+{
+	bMouseDown_ = false;
+	bDragging_ = false;
+
+	FEventReply Reply(true);
+	Reply.NativeReply = FReply::Handled().ReleaseMouseCapture();
+	return Reply;
 }
--- Source/Warp/CampaignMap/MapViewportWidget.h
+++ Source/Warp/CampaignMap/MapViewportWidget.h
@@ -20,12 +20,37 @@ class WARP_API UMapViewportWidget : public UUserWidget
 protected:
 	virtual void NativeConstruct() override;
 
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+
+	UFUNCTION()
+	FEventReply OnCatcherMouseDown(FGeometry MyGeometry, const FPointerEvent& MouseEvent);
+
+	UFUNCTION()
+	FEventReply OnCatcherMouseMove(FGeometry MyGeometry, const FPointerEvent& MouseEvent);
+
+	UFUNCTION()
+	FEventReply OnCatcherMouseUp(FGeometry MyGeometry, const FPointerEvent& MouseEvent);
+
 	UPROPERTY(meta = (BindWidget))
 	TObjectPtr<UBorder> InputCatcher;
 	
 	UPROPERTY(meta = (BindWidget))
-	TObjectPtr<class UBorder> MapBorder;
+	TObjectPtr<UBorder> MapBorder;
 	
 	UPROPERTY(meta = (BindWidget))
 	TObjectPtr<UCanvasPanel> MapContentRoot;
+	
+	UPROPERTY(EditAnywhere, Category="MapViewport")
+	float DragThreshold_ = 10;
+	
+	UPROPERTY(EditAnywhere, Category="MapViewport")
+	float InterpSpeed_ = 10;
+
+	bool bMouseDown_ = false;
+	bool bDragging_ = false;
+	
+	FVector2f PressPos_ = FVector2f::ZeroVector;
+	FVector2f LastPos_ = FVector2f::ZeroVector;
+	FVector2D TargetOffset_ = FVector2D::ZeroVector;
+	FVector2D CurrentOffset_ = FVector2D::ZeroVector;
 };
