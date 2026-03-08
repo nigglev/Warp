48097a81094dfdb29908df82fe8159fbbdaf6170
Ivan Ekimov <iaekimov96@gmail.com>
2026-03-08 17:23:44 +0100

    UI TurnOrder first version with bugs

---
+++ Source/Warp/UI/CombatUI/TurnOrderUI/TurnOrderEntryWidget.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UTurnOrderEntryWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARP_API UUTurnOrderEntryWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};
---
+++ Source/Warp/UI/CombatUI/TurnOrderUI/TurnOrderWidget.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UTurnOrderWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARP_API UUTurnOrderWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};
